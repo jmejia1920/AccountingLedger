d906c0260df77fffd553a4a9c4a9c7a3c22fad44
jmejia1920 <jonny86493@gmail.com>
2023-05-01 13:56:23 -0400

    First commit

---
+++ src/main/java/com/learntocode/FinancialTransactions.java
@@ -0,0 +1,40 @@
+package com.learntocode;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FinancialTransactions {
+    public static void main(String[] args) {
+        ArrayList<Product> transactions = getTransactions();
+
+        for (Product product : transactions) {
+            System.out.println(product);
+        }
+        Scanner scanner = new Scanner(System.in);
+        int command = 0;
+        while(command != 5){
+            System.out.println("What would you like to do?");
+            System.out.println("1- Add deposit");
+            System.out.println("2- Make Payment (Debit)");
+            System.out.println("3- Ledger");
+            System.out.println("4- Exit");
+
+            System.out.print("Enter command: ");
+            command = scanner.nextInt();
+
+            switch (command) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    System.out.println("Thank You!\nHave a Good Day!!");
+            }
+        }
+
+    }
+    private static ArrayList<Product> getTransactions(){
+        ArrayList<Product> transactions = new ArrayList<>();
+        String line;
+        return transactions;
+    }
+}
---
+++ src/main/java/com/learntocode/Product.java
@@ -0,0 +1,47 @@
+package com.learntocode;
+
+public class Product {
+    private String date;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Product(String date, String description, String vendor, double amount) {
+        this.date = date;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
